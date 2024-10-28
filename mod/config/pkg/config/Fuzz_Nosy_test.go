8fa248bdee208bc4b36bbd6ac4a3c5d21d6169b3
David Theodore <prodigalsonsolutions@gmail.com>
2024-10-28 18:05:21 +0100

    config fuzzers

---
+++ mod/config/pkg/config/Fuzz_Nosy_test.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"testing"
+
+	"github.com/spf13/viper"
+	go_fuzz_utils "github.com/trailofbits/go-fuzz-utils"
+)
+
+func GetTypeProvider(data []byte) (*go_fuzz_utils.TypeProvider, error) {
+	tp, err := go_fuzz_utils.NewTypeProvider(data)
+	if err != nil {
+		return nil, err
+	}
+	err = tp.SetParamsStringBounds(0, 1024)
+	if err != nil {
+		return nil, err
+	}
+	err = tp.SetParamsSliceBounds(0, 4096)
+	if err != nil {
+		return nil, err
+	}
+	err = tp.SetParamsBiases(0, 0, 0, 0)
+	if err != nil {
+		return nil, err
+	}
+	return tp, nil
+}
+
+func Fuzz_Nosy_Config_ValidateBasic__(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+
+		tp, fill_err := GetTypeProvider(data)
+		if fill_err != nil {
+			return
+		}
+		var v *viper.Viper
+		fill_err = tp.Fill(&v)
+		if fill_err != nil {
+			return
+		}
+		if v == nil {
+			return
+		}
+
+		c, err := ParseConfig(v)
+		if err != nil {
+			return
+		}
+		c.ValidateBasic()
+	})
+}
